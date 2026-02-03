07b02c77ec6c014ada7ba331a94d5b7d1a2025db
Магомед Муртузаев <58600545+Overlord5465@users.noreply.github.com>
2026-02-03 05:21:47 +0300

    Delete mysite directory

--- mysite/works/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, register_converter
-from . import views
-
-urlpatterns = [
-    path('', views.SearchWorksView.as_view(), name='home'),  # http://127.0.0.1:8000
-    path('list_teacher/', views.ShowTeachersView.as_view(), name='list_teacher'),
-    # path('post/<slug:post_slug>/', views.ShowPost.as_view(), name='post'),
-    # path('edit/<slug:slug>/', views.UpdatePage.as_view(), name='edit_page'),
-]
